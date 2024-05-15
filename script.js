71c80772867754d208546c79043653603c6f28bf
sabibulla <kanagalakshmi4306@gmail.com>
2024-05-15 17:46:35 +0530

    styles are added

---
+++ script.js
@@ -0,0 +1,19 @@
+// const btncart=document.querySelector('.carticon') ;
+// const cart=document.querySelector('.cart-slider');
+// const btnclose=document.querySelector('#cart-close');
+
+// btncart.addEventListener('click',()=>{
+    
+//     cart.classList.add('cart-active')
+
+// });
+
+// btnclose.addEventListener('click',()=>{
+//     cart.classList.remove('cart-active')
+//     console.log(cart.classList.remove('cart-active'))
+
+// });
+// const btnCart = document.querySelector('.carticon');
+// const cart = document.querySelector('cart-slider');
+// const btnClose = document.querySelector('#cart-close');
+// console.log (cart)
