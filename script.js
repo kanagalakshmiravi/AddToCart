6063591592196e94c617e534b7bfd7a168330d94
sabibulla <kanagalakshmi4306@gmail.com>
2024-05-16 22:59:47 +0530

    js added

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
-// const btncart=document.querySelector('.carticon') ;
-// const cart=document.querySelector('.cart-slider');
-// const btnclose=document.querySelector('#cart-close');
+const btncart=document.getElementById('cart-icon');
+const cartslide=document.querySelector('.cart-slider');
+const btnclose=document.getElementById('cart-close');
 
-// btncart.addEventListener('click',()=>{
-    
-//     cart.classList.add('cart-active')
+btncart.addEventListener('click',()=>{
+    cartslide.classList.add('cart-active');
+});
 
-// });
+btnclose.addEventListener('click',()=>{
+    cartslide.classList.remove('cart-active');
+});
 
-// btnclose.addEventListener('click',()=>{
-//     cart.classList.remove('cart-active')
-//     console.log(cart.classList.remove('cart-active'))
+document.addEventListener('DOMContentLoaded',loadfood);
 
-// });
-const btnCart = document.querySelector('.carticon');
-const cart = document.querySelector('cart-slider');
-const btnClose = document.querySelector('#cart-close');
-console.log (cart)
+function loadfood(){
+    loadcontent();
+}
+
+function loadcontent(){
+    //remove food items from cart
+    let btnremove=document.querySelectorAll('.cart-remove');
+    btnremove.forEach((btn)=>{
+      btn.addEventListener('click',removeitem) ;
+    });
+
+    // product item change event
+    let qtyelement=document.querySelectorAll('.cart-qut');
+    btnremove.forEach((input)=>{
+      btn.addEventListener('click',Quantitychange) ;
+    });
+}
+
+//remove items 
+function removeitem(){
+    this.parentElement.remove();
+}
+
+//change quantity
+
+// function Quantitychange(){
+//     if(isNaN(this.value) || this.value<1){
+//         this.value=1;
+//     }
+// }
