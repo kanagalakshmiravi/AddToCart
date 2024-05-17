6943a68a693888a1dca44fc146c69653c90f8563
sabibulla <kanagalakshmi4306@gmail.com>
2024-05-17 20:30:08 +0530

    js added

--- script.js
+++ script.js
@@ -28,12 +28,73 @@ function loadcontent(){
     qtyelement.forEach((input)=>{
       input.addEventListener('change',changeqty);
     });
+
+    //product cart
+    let cartbtns=document.querySelectorAll('.foodcart');
+    cartbtns.forEach((btn)=>{
+      btn.addEventListener('click',addcart) ;
+    });
+
+
 }
 
 //remove items 
 function removeitem(){
+    if (confirm('Are you sure to remove'))
     this.parentElement.remove();
 }
 
 //change quantity
+
+function changeqty(){
+    if(isNaN(this.value) || this.value<1){
+   this.value=1;
+    }
+}
+
+//add cart
+function addcart(){
+   let food =this.parentElement;
+   let title = food.querySelector('.foodname').innerHTML;
+   let price= food.querySelector('.food-price').innerHTML;
+   let imgSrc = food.querySelector('.foodimage').src;
+
+//    console.log(title,price,imgSrc)
+ let productelement= createcartproduct(title,price,imgSrc); 
+ let element=document.createElement('div');
+ element.innerHTML=productelement;
+ let cartbasket=document.querySelector('.cart-content');
+ cartbasket.append(productelement)
+  loadcontent()
+   
+   
+}
+
+function createcartproduct(title,price,imgSrc){
+
+return `
+<div class="cart-box">
+                            <img src="${imgSrc}" alt="" class="cart-image">
+                            <div class="detail-box">
+                                <div class="food-name">${title}</div>
+                                <div class="price-box">
+                                    <div class="cart-price">${price}</div>
+                                    <div class="cart-amt">${price}</div>
+                                </div>
+                                <input type="number" value="1" class="cart-qut">
+
+                            </div>
+                            <ion-icon name="trash" class="cart-remove"></ion-icon>
+                        </div>
+`;
+
+}
+
+
+
+
+
+
+
+
 
